da77ffb6699340a7a544759094be57aaeebc5d3c
Mark Andrews <mjandrews.org@gmail.com>
2021-11-04 16:03:33 +0000

    Update scripts and recordings

--- scripts/script_iglm04_day2.R
+++ scripts/script_iglm04_day2.R
@@ -68,3 +68,96 @@ deviance(M9) - deviance(M10)
 pchisq(deviance(M9) - deviance(M10), df = 3, lower.tail = FALSE)
 
 anova(M9, M10, test = 'Chisq')
+
+# Exposure and offsets ----------------------------------------------------
+
+insurance_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/iglm04/main/data/Insurance.csv")
+
+
+M11 <- glm(Claims ~ Age + offset(log(Holders)),
+           data = insurance_df,
+           family = poisson(link = 'log')
+)
+
+insurance_df_2 <- tibble(Age = unique(insurance_df$Age),
+                         Holders = 1000)
+
+predict(M11, newdata = insurance_df_2)
+
+library(modelr)
+
+add_predictions(insurance_df_2, M11)
+add_predictions(insurance_df_2, M11, type = 'response')
+
+
+# binomial count models ---------------------------------------------------
+
+golf_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/iglm04/main/data/golf_putts.csv")
+
+M12 <- glm(cbind(success, attempts - success) ~ distance,
+           data = golf_df,
+           family = binomial(link = 'logit')
+)
+
+summary(M12)$coefficients
+coef(M12)
+
+golf_df_2 <- tibble(distance = seq(20))
+
+add_predictions(golf_df_2, M12, var = 'log_odds_successful_putt')
+add_predictions(golf_df_2, M12, type = 'response', var = 'probability_successful_putt')
+
+
+add_predictions(golf_df_2, 
+                M12, 
+                type = 'response', 
+                var = 'probability_successful_putt') %>% 
+  ggplot(
+    aes(x = distance, y = probability_successful_putt)
+  ) + geom_line()
+
+# Overdispersion ----------------------------------------------------------
+
+# samples some data from a Poisson distribution
+x <- rpois(1000, lambda = 100)
+mean(x)/var(x)
+
+biochem_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/iglm04/main/data/biochemist.csv")
+
+M13 <- glm(publications ~ 1,
+           data = biochem_df,
+           family = poisson(link = 'log')
+)
+summary(M13)$coefficients
+exp(0.52)
+
+pubs <- biochem_df$publications
+c(mean(pubs), var(pubs), var(pubs)/mean(pubs))
+
+
+
+M14 <- glm(publications ~ 1,
+           data = biochem_df,
+           family = quasipoisson(link = 'log')
+)
+
+summary(M14)$coefficients
+
+
+# Negative binomial regression --------------------------------------------
+
+library(MASS)
+
+M15 <- glm.nb(publications ~ prestige,
+              data = biochem_df)
+
+summary(M15)
+estimates <- coef(M15)
+
+# predicted log of the mean of the negbin for values of 
+# prestige from 1 to 5
+estimates[1] + estimates[2] * seq(5)
+
+# predicted mean of the negbin for values of 
+# prestige from 1 to 5
+exp(estimates[1] + estimates[2] * seq(5))
